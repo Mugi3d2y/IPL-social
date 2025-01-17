047c5cce751142226c3a61745bdf9fcae1831679
Marouan <marouan.yaouaddou@student.vinci.be>
2025-01-17 17:08:28 +0100

    add: should return true if the number of characters is >= 8

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -8,4 +8,10 @@ describe('isPasswordValid should return true for a valid password and false for 
 
         expect(result).toBe(false);
     });
+
+    it('should return true if the number of characters is >= 8', function() {
+        const result = main.isPasswordValid('12345678');
+
+        expect(result).toBe(true);
+    });
 }); 
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,8 +1,6 @@
 export class Main {
 
     isPasswordValid(password) {
-        console.log("" + password.length);
-        
         if(password.length < 8) {
             return false;
         }
